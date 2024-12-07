652366548e6e4bd2d0f87b6aa5069155567f4e7c
Marien van Os <marien@minglesports.tech>
2024-12-07 22:22:15 +0100

    test

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -6,18 +6,7 @@ export function Welcome({ message }: { message: string }) {
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
-            <img
-              src={logoLight}
-              alt="React Router"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src={logoDark}
-              alt="React Router"
-              className="hidden w-full dark:block"
-            />
-          </div>
+          Joostvanos.nl
         </header>
         <div className="max-w-[300px] w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
