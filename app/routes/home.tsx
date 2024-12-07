408418f3134bf869642541c23c67f0648ec760ac
Marien van Os <marien@minglesports.tech>
2024-12-07 22:49:59 +0100

    meta

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Joost van Os Makelaardij & Mediation" },
+    { name: "description", content: "Welcome to joostvanos.nl!" },
   ];
 }
 
