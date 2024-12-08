172ad5f240ca818eb6ade0eae42875d431d2e94f
Marien van Os <marien@minglesports.tech>
2024-12-08 14:45:10 +0100

    width

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -6,8 +6,8 @@ export function Welcome({ message }: { message: string }) {
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
 
-        <div className="max-w-[300px] w-full space-y-6 px-4">
-          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4 w-[500px]">
+        <div className="max-w-[400px] w-full space-y-6 px-4">
+          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4 ">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
 
               Joost van Os Makelaardij & Mediation<br/>
