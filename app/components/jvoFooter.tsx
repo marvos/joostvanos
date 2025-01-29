def19cee506bb1149c87a291987d9c855b8b5feb
Marien van Os <marien@minglesports.tech>
2025-01-29 22:06:17 +0100

    footer for mobile

--- app/components/jvoFooter.tsx
+++ app/components/jvoFooter.tsx
@@ -1,7 +1,7 @@
 export function JvoFooter() {
   return (
     <div className="bg-mocha-900 text-mocha-50 p-10">
-      <footer className="footer  p-10  container grid grid-cols-4">
+      <footer className="footer  p-10  container flex flex-col lg:grid lg:grid-cols-4">
         <nav>
           <h6 className="footer-title">Services</h6>
           <a className="link link-hover">Makelaardij</a>
@@ -63,47 +63,46 @@ export function JvoFooter() {
               0622 691573
             </a>
           </div>
-            <a
-              href="mailto:makelaardij@joostvanos.nl"
-              className="btn btn-link px-0"
+          <a
+            href="mailto:makelaardij@joostvanos.nl"
+            className="btn btn-link px-0"
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g clipPath="url(#clip0_17_19831)">
-                  <path
-                    d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22H17V20H12C7.66 20 4 16.34 4 12C4 7.66 7.66 4 12 4C16.34 4 20 7.66 20 12V13.43C20 14.22 19.29 15 18.5 15C17.71 15 17 14.22 17 13.43V12C17 9.24 14.76 7 12 7C9.24 7 7 9.24 7 12C7 14.76 9.24 17 12 17C13.38 17 14.64 16.44 15.54 15.53C16.19 16.42 17.31 17 18.5 17C20.47 17 22 15.4 22 13.43V12C22 6.48 17.52 2 12 2ZM12 15C10.34 15 9 13.66 9 12C9 10.34 10.34 9 12 9C13.66 9 15 10.34 15 12C15 13.66 13.66 15 12 15Z"
-                    fill="currentColor"
-                  />
-                </g>
-              </svg>
-              makelaardij@joostvanos.nl
-            </a>
-            <a
-              href="mailto:mediation@joostvanos.nl"
-              className="btn btn-link px-0"
+              <g clipPath="url(#clip0_17_19831)">
+                <path
+                  d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22H17V20H12C7.66 20 4 16.34 4 12C4 7.66 7.66 4 12 4C16.34 4 20 7.66 20 12V13.43C20 14.22 19.29 15 18.5 15C17.71 15 17 14.22 17 13.43V12C17 9.24 14.76 7 12 7C9.24 7 7 9.24 7 12C7 14.76 9.24 17 12 17C13.38 17 14.64 16.44 15.54 15.53C16.19 16.42 17.31 17 18.5 17C20.47 17 22 15.4 22 13.43V12C22 6.48 17.52 2 12 2ZM12 15C10.34 15 9 13.66 9 12C9 10.34 10.34 9 12 9C13.66 9 15 10.34 15 12C15 13.66 13.66 15 12 15Z"
+                  fill="currentColor"
+                />
+              </g>
+            </svg>
+            makelaardij@joostvanos.nl
+          </a>
+          <a
+            href="mailto:mediation@joostvanos.nl"
+            className="btn btn-link px-0"
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g clipPath="url(#clip0_17_19831)">
-                  <path
-                    d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22H17V20H12C7.66 20 4 16.34 4 12C4 7.66 7.66 4 12 4C16.34 4 20 7.66 20 12V13.43C20 14.22 19.29 15 18.5 15C17.71 15 17 14.22 17 13.43V12C17 9.24 14.76 7 12 7C9.24 7 7 9.24 7 12C7 14.76 9.24 17 12 17C13.38 17 14.64 16.44 15.54 15.53C16.19 16.42 17.31 17 18.5 17C20.47 17 22 15.4 22 13.43V12C22 6.48 17.52 2 12 2ZM12 15C10.34 15 9 13.66 9 12C9 10.34 10.34 9 12 9C13.66 9 15 10.34 15 12C15 13.66 13.66 15 12 15Z"
-                    fill="currentColor"
-                  />
-                </g>
-              </svg>
-              mediation@joostvanos.nl
-            </a>
-
+              <g clipPath="url(#clip0_17_19831)">
+                <path
+                  d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22H17V20H12C7.66 20 4 16.34 4 12C4 7.66 7.66 4 12 4C16.34 4 20 7.66 20 12V13.43C20 14.22 19.29 15 18.5 15C17.71 15 17 14.22 17 13.43V12C17 9.24 14.76 7 12 7C9.24 7 7 9.24 7 12C7 14.76 9.24 17 12 17C13.38 17 14.64 16.44 15.54 15.53C16.19 16.42 17.31 17 18.5 17C20.47 17 22 15.4 22 13.43V12C22 6.48 17.52 2 12 2ZM12 15C10.34 15 9 13.66 9 12C9 10.34 10.34 9 12 9C13.66 9 15 10.34 15 12C15 13.66 13.66 15 12 15Z"
+                  fill="currentColor"
+                />
+              </g>
+            </svg>
+            mediation@joostvanos.nl
+          </a>
 
           {/*<ul>*/}
           {/*  {resources.map(({ href, text, icon }) => (*/}
